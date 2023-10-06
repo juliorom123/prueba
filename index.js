3a1851137e1211c09be2ece3fc00c89e8b877268
julio <jromero@itpfp.com>
2023-10-06 14:36:35 +0200

    Soy gay

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const path = require('path');
+
+app.use(express.static(__dirname));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.resolve(__dirname, 'index.html'));
+})
+
+
+app.listen(3000);
